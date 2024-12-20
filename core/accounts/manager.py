529e8ce82fcd006135948de7fb7c85ddfce28888
Prathmesh Pandya <phpwork1414@gmail.com>
2024-12-20 15:03:18 +0530

    Add files via upload

---
+++ core/accounts/manager.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError("Phone Number is required")
+       
+        # email = extra_fields.get('email')
+        # if email:
+        #     extra_fields['email'] = self.normalize_email(email)
+        user = self.model(phone_number=phone_number, **extra_fields) 
+        print(password)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        print(password)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        return self.create_user(phone_number, password, **extra_fields)
+   
---
+++ core/accounts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .manager import UserManager
+
+class CustomUser(AbstractUser):
+    username = None
+    phone_number = models.CharField(max_length=50, unique=True)
+    # email = models.EmailField(unique=False)
+    # user_bio = models.CharField(max_length=50)
+    # user_profile_image = models.ImageField(upload_to='profile')
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+
+
